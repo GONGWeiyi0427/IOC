76207238bf83681bfdab2b54c7481c51e021514f
Samuel <94922608+GONGWeiyi0427@users.noreply.github.com>
2023-05-19 13:27:22 +0200

    Add files via upload

--- projet/client.py
+++ projet/client.py
@@ -1,33 +1,30 @@
-import paho.mqtt.client as mqttClient
+import paho.mqtt.client as mqtt
 
-# Callback function to handle incoming MQTT messages and write them to a text file
+# Callback function to handle incoming MQTT messages and write the luminosity value to a text file
 def on_message(client, userdata, message):
-    with open('mqtt_lum.txt', 'a') as file:
-        file.write(f"Topic: {message.topic}\n")
-        file.write(f"Message: {message.payload.decode()}\n\n")
+    if message.topic == "topic_lum":
+        luminosity = message.payload.decode()
+        with open('luminosity.txt', 'w') as file:
+            file.write(luminosity)
 
-# Create an MQTT client instance and set the callback function
-client = mqttClient.Client()
+# Create an MQTT client instance
+client = mqtt.Client()
 
 # Set the username and password for the MQTT broker
 broker_username = "gong"
 broker_password = "weiyigong"
 client.username_pw_set(broker_username, broker_password)
 
+# Set the callback function
 client.on_message = on_message
 
-# Connect to the MQTT broker and subscribe to the desired topic(s)
-broker_address = "172.20.10.8"  # Replace with your broker address
-topic = "topic_lum"  # Replace with the topic you want to subscribe to
+# Connect to the MQTT broker and subscribe to the desired topic
+broker_address = "172.20.10.8"
+broker_port = 1883
+topic = "topic_lum"
 
-client.connect(broker_address)
+client.connect(broker_address, broker_port)
 client.subscribe(topic)
-client.loop_start()
 
-try:
-    # Run the MQTT client and wait for incoming messages
-    client.loop_forever()
-except KeyboardInterrupt:
-    # Disconnect the MQTT client on program termination
-    client.disconnect()
-    client.loop_stop()
+# Run the MQTT client in a loop to receive messages
+client.loop_forever()
