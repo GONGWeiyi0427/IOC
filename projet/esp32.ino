f0407d22e27f68a4afd5d1b1a56cab9fa9b14bf8
Samuel <94922608+GONGWeiyi0427@users.noreply.github.com>
2023-05-21 01:28:49 +0200

    Add files via upload

--- projet/esp32.ino
+++ projet/esp32.ino
@@ -1,150 +1,154 @@
-#include <WiFi.h>
-#include <PubSubClient.h>
-
+#include <WiFi.h>                 // Library for wifi
+#include <PubSubClient.h>         // Library for MQTT communication
+  
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-const char* ssid = "Damn";
-const char* password = "fuckinggood";
+const char* ssid = "Damn";        // Username of WIFI
+const char* password = "fuckinggood";   // Password for WIFI
+  
+const char* mqtt_server = "172.20.10.8";    // IP of MQTT
+const char* mqtt_user = "gong";             // Username of MQTT
+const char* mqtt_password = "weiyigong";    // Password of MQTT
 
-const char* mqtt_server = "172.20.10.8";
-const char* mqtt_user = "gong";
-const char* mqtt_password = "weiyigong";
+WiFiClient wifiClient;                      // Creation of wifi client
 
-WiFiClient wifiClient;
-
-PubSubClient client(mqtt_server,1883,wifiClient);
+PubSubClient client(mqtt_server, 1883, wifiClient);   // Create an instance of the PubSubClient class to handle MQTT communication
 
 #define sensor_pin  36
 
-#define MAX_WAIT_FOR_TIMER 5  //Definition du nombre de timer
+#define MAX_WAIT_FOR_TIMER 5  // Definition of the number of timers
 
-enum {EMPTY, FULL}; //Definition pour les flags
+enum {EMPTY, FULL}; // Definition for the flags
 
-struct mailbox_s {    //Structure de mailbox
-  int state;        //State soit EMPTY, soit FULL
-  int val;          //La valeur de mailbox qui a besoin de communiquer entre les different taches
+struct mailbox_s {    // Structure of the mailbox
+  int state;        // State either EMPTY or FULL
+  int val;          // The value of the mailbox that needs to be communicated between different tasks
 };
 
-struct mailbox_s mb_publish = {.state = EMPTY};     //Initialisation de mailbox pour l'intensite de la lumiere
+struct mailbox_s mb_publish = {.state = EMPTY};     // Initialization of the mailbox for the light intensity
 
-struct lum{
+struct lum {
   int timer;
   unsigned long period;
 };
 
 struct lum lum_publish;
 
-unsigned int waitFor(int timer, unsigned long period){
-  static unsigned long waitForTimer[MAX_WAIT_FOR_TIMER];  // il y a autant de timers que de tâches périodiques
-  unsigned long newTime = micros() / period;              // numéro de la période modulo 2^32 
-  int delta = newTime - waitForTimer[timer];              // delta entre la période courante et celle enregistrée
-  if ( delta < 0 ) delta = 1 + newTime;                   // en cas de dépassement du nombre de périodes possibles sur 2^32 
-  if ( delta ) waitForTimer[timer] = newTime;             // enregistrement du nouveau numéro de période
+unsigned int waitFor(int timer, unsigned long period) {
+  static unsigned long waitForTimer[MAX_WAIT_FOR_TIMER];  // There are as many timers as periodic tasks
+  unsigned long newTime = micros() / period;              // Number of periods modulo 2^32 
+  int delta = newTime - waitForTimer[timer];              // Delta between the current period and the recorded one
+  if (delta < 0) delta = 1 + newTime;                     // In case the number of periods exceeds 2^32 
+  if (delta) waitForTimer[timer] = newTime;               // Recording the new period number
   return delta;
 }
 
-void setup_lum(struct lum *lum_publish, int timer, unsigned long period)//Setup de timer pour recuperer l'intensite de la lumiere
-{
+void setup_lum(struct lum *lum_publish, int timer, unsigned long period) {
   lum_publish->timer = timer;
   lum_publish->period = period;
 }
 
-int readLum()                                            // La fonction pour recupere l'intensite de la lumiere
-{
-  int lum = analogRead(sensor_pin);                        // Lire la valeur dans le pin de sensor
+int readLum() {
+  int lum = analogRead(sensor_pin);                        // Read the value from the sensor pin
   lum = map(lum, 0, 4096, 100, 0);
   return lum;
 }
 
 void loop_Lum(struct mailbox_s *mb_publish, struct lum *lum_publish) {
-  if (!(waitFor(lum_publish->timer,lum_publish->period))) return;  //Si le temps n'est pas passe une periode
+  if (!(waitFor(lum_publish->timer, lum_publish->period))) return;  // If the time hasn't passed one period
   int lumvalue = readLum();
   
   if (mb_publish->state != FULL) {
-    mb_publish->val = lumvalue;    // On peut directement envoyer la valeur vers oled
-    mb_publish->state = FULL; // Laisse le flag de mailbox en FULL
+    mb_publish->val = lumvalue;    // The value can be directly sent to OLED
+    mb_publish->state = FULL; // Set the mailbox flag to FULL
   }
 }
 
-
-void setup_wifi()
-{
+void setup_wifi() {
   delay(10);
   Serial.println();
-
-  WiFi.begin(ssid,password);
+  
+  WiFi.begin(ssid, password);  // Connect with WIFI
   Serial.println("\nConnecting");
-
-  while(WiFi.status() != WL_CONNECTED)
+  
+  while (WiFi.status() != WL_CONNECTED)    // While waiting for the connection to the WIFI
   {
     Serial.print(".");
     delay(100);
   }
-
-  Serial.println("\nConnected to WiFi network");
+  
+  // After connection
+  Serial.println("\nConnected to WiFi network");    
   Serial.print("Local ESP32 IP: ");
   Serial.println(WiFi.localIP());
-  
 }
 
-void connect_rasp()
-{
-
-  while(!client.connected())
-  {
-    if(client.connect("ESP32CLIENT",mqtt_user,mqtt_password))
+void connect_rasp() {
+  while (!client.connected()) {
+    if (client.connect("ESP32CLIENT", mqtt_user, mqtt_password))   // Connect to MQTT
     {
       Serial.println("Connected with Raspb");
+      //client.subscribe("topic_buzzer",mqtt_user,mqtt_password);
+      //client.subscribe("topic_led",mqtt_user,mqtt_password);
     }
-    else
-    {
-      Serial.print("failed,rc=");
+    else {
+      Serial.print("failed, rc=");
       Serial.print(client.state());
       delay(5000);
     }
   }
 }
 
+/*
+void callback(const char* topic, byte* payload, unsigned int length) {
+  String Topic = String(topic);
+  Serial.println(Topic);
+  Serial.print("length message received in callback");
+  Serial.println(length);
+  int i;
+  for (i = 0; i < length; i++) {
+    buf[i] = (char)payload[i];
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+  buf[i] = '\0';
+  String str = String(buf);
+
+  if (Topic == "topic_buzzer") {
+
+  }
+  else if (Topic == "topic_led") {
+
+  }
+}
+*/
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  Wire.begin(4,15);
-
-  setup_lum(&lum_publish,0,500000);//Timer 1 et periode 5000000
+  Wire.begin(4, 15);
+  
+  setup_lum(&lum_publish, 0, 500000);  // Timer 1 and period 5000000
   
   setup_wifi();
-  client.setServer(mqtt_server,1883);
-  //client.setCallback();
-
-
-  
+  client.setServer(mqtt_server, 1883);
+  //client.setCallback(callback);
 }
 
 void loop() {
-  if(!client.connected())
+  if (!client.connected())   // If MQTT is not connected
   {
-      connect_rasp();
+    connect_rasp();       // Reconnect MQTT
   }
-  // put your main code here, to run repeatedly:
-  //attachInterrupt(digitalPinToInterrupt(2), serialEvent, CHANGE);
-  //attachInterrupt(digitalPinToInterrupt(3), serialEvent, CHANGE);//Charger 2 fonction pour attach l'interrupteurs en boucle chaque fois
-  //loop_readLum(&mb_lum,&lum1,&mb_led);
-  //loop_afficheLum(&mb_lum);
-  //oledCount(&counter1);
-  //loop_ledLum(&mb_led,&led,&mb_isr);
-  //loop_bp(&bp,&mb_buzzer);
-  //loop_buzzer(&buzzer,&mb_buzzer);//Generer les fonctions
 
-  loop_Lum(&mb_publish,&lum_publish);
+  loop_Lum(&mb_publish, &lum_publish);   // Loop to read the luminosity
 
-  if(mb_publish.state != EMPTY)
-  {
-    String lum = (String)(mb_publish.val);
-    client.publish("topic_lum",lum.c_str());
+  if (mb_publish.state != EMPTY) {
+    String lum = (String)(mb_publish.val);    // Convert the value to string for the message to be sent
+    client.publish("topic_lum", lum.c_str());  // Send the message to MQTT
     mb_publish.state = EMPTY;
   }
 }
